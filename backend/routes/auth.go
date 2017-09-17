fb6f35bbc19890bf30372f166e6d0e7a35208c5a
Alex <thefamily.guo@gmail.com>
2017-09-17 00:19:47 -0400

    copy pasta nick wus code, turntechGodhead

--- backend/routes/auth.go
+++ backend/routes/auth.go
@@ -44,3 +44,45 @@ func GetLogin(key []byte) func(c *gin.Context) {
 		c.JSON(http.StatusAccepted, gin.H{"token": token})
 	}
 }
+
+func RegisterUser(key []byte) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		session, exists := c.Get("db")
+		if !exists {
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		s, valid := session.(*mgo.Session)
+		if !valid {
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+		}
+
+		exists, err := db.CheckUserExists(s, user.Username)
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		if exists {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		} else {
+			err := db.InsertUser(s, user)
+
+			if err != nil {
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+		}
+
+		token, err := middleware.GetToken(user.Username, key)
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		c.JSON(http.StatusAccepted, gin.H{"token": token})
+	}
+}
